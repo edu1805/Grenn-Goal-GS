4ee287868fb445d054a5d6499c5f9b33ef871ba3
edu1805 <eduardobarriviera1805@gmail.com>
2024-11-22 17:17:47 -0300

    Correção da api de eletrodomestico

--- src/app/api/eletrodomestico/route.ts
+++ src/app/api/eletrodomestico/route.ts
@@ -4,7 +4,6 @@ export async function POST(request: Request) {
     try {
         const { nome, tipo, potencia, horasUsoDiarias, quantidade } = await request.json();
         const residenciaId = 5
-
         
         const novoEletrodomestico = {
             nome,
@@ -15,14 +14,12 @@ export async function POST(request: Request) {
             residenciaId
         };
 
-        // Faz a requisição para a API externa
         const apiResponse = await fetch("http://localhost:8080/greengoalproject_war/api/eletrodomestico", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify(novoEletrodomestico),
         });
 
-        // Verifica se a requisição foi bem-sucedida
         if (!apiResponse.ok) {
             console.log(`Erro na API: ${apiResponse.statusText}`);
             return NextResponse.json(
@@ -31,10 +28,8 @@ export async function POST(request: Request) {
             );
         }
 
-        // Obtém o corpo da resposta da API externa
         const createdEletro = await apiResponse.json();
 
-        // Retorna o resultado para o cliente
         return NextResponse.json(createdEletro);
     } catch (error) {
         console.error("Erro ao cadastrar elétrodomestico:", error);
